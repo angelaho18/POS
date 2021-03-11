6b1e7fbd3c8f05e485bf721b6b51e0a2cb0cc276
angelaho18 <angelllaaaaaaaa@gmail.com>
2021-03-11 15:50:24 +0700

    camera

--- app/src/main/java/com/example/pointofsale/Profile.kt
+++ app/src/main/java/com/example/pointofsale/Profile.kt
@@ -1,8 +1,16 @@
 package com.example.pointofsale
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -21,4 +29,35 @@ class Profile : AppCompatActivity() {
             startActivity(intent_logout)
         }
     }
+    fun opencamera(view: View){
+        cameraPermission()
+        //ambil gambar
+        var gambar = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(gambar.resolveActivity(packageManager)!=null){
+            startActivityForResult(gambar,181)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //data kembali
+        if(requestCode == 181 && resultCode==Activity.RESULT_OK && data !=null){
+            var thumbnail = data.extras
+            gambarProfile.setImageBitmap(thumbnail?.get("data")as Bitmap)
+        }
+    }
+    private fun cameraPermission() {
+        //cek permission
+        var permission = arrayOf(Manifest.permission.CAMERA)
+        var checkNeed : ArrayList<String> = ArrayList()
+        for(x in permission){
+            if(ContextCompat.checkSelfPermission(this@Profile,x) != PackageManager.PERMISSION_GRANTED){
+                checkNeed.add(x)
+            }
+        }
+        if(!checkNeed.isEmpty()){
+            ActivityCompat.requestPermissions(this,checkNeed.toArray(arrayOfNulls(checkNeed.size)),18)
+        }
+    }
+
 }
