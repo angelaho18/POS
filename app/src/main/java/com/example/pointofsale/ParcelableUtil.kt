fa1b2ad07bb395c449b48cced1308d8690387c2e
angelaho18 <angelllaaaaaaaa@gmail.com>
2021-04-07 21:52:05 +0700

    alarm manager

---
+++ app/src/main/java/com/example/pointofsale/ParcelableUtil.kt
@@ -0,0 +1,24 @@
+package com.example.pointofsale
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.ArrayList
+
+object ParcelableUtil {
+    fun marshall(parceable: Parcelable): ByteArray {
+        val parcel = Parcel.obtain()
+        parceable.writeToParcel(parcel, 0)
+        val bytes = parcel.marshall()
+        parcel.recycle()
+        return bytes
+    }
+
+    fun unmarshall(bytes: ByteArray): Parcel {
+        val parcel = Parcel.obtain()
+        parcel.unmarshall(bytes, 0, bytes.size)
+        parcel.setDataPosition(0) // This is extremely important!
+        return parcel
+    }
+
+
+}
