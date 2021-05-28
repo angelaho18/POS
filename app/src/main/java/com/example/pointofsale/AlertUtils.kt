18189d0d01ad4f86b281502ebc0be8235107dc42
Shelly Tansil <shellytansil93@gmail.com>
2021-05-28 19:48:32 +0700

    Room - edit, delete data

---
+++ app/src/main/java/com/example/pointofsale/AlertUtils.kt
@@ -0,0 +1,46 @@
+package com.example.pointofsale
+
+import android.app.AlertDialog
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.example.pointofsale.fragments.fragment_list
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+object AlertUtils {
+    fun updataDialog(view: View, item: Product, db: ProductDBHelper){
+        val views = View.inflate(view.context, R.layout.layout_pop_up, null)
+        val builder = AlertDialog.Builder(view.context)
+        builder.setView(views)
+        var dialog = builder.create()
+        val confirmBtn = views.findViewById<Button>(R.id.bt_add_to_conf)
+        val cancelBtn = views.findViewById<Button>(R.id.bt_cancel)
+        val name = views.findViewById<EditText>(R.id.inputName)
+        val qty = views.findViewById<EditText>(R.id.inputQty)
+        val price = views.findViewById<EditText>(R.id.inputPrice)
+
+        confirmBtn.text = "Update"
+
+        name.setText(item.ProductName)
+        qty.setText(item.Quantity.toString())
+        price.setText(item.Price.toString())
+
+        dialog.show()
+        confirmBtn.setOnClickListener {
+            doAsync{
+                item.ProductName = name.text.toString()
+                item.Quantity = qty.text.toString().toInt()
+                item.Price = price.text.toString().toInt()
+                db.productDao().updateData(item)
+                uiThread {
+                    fragment_list.refreshData(db)
+                }
+            }
+            dialog.dismiss()
+        }
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+}
