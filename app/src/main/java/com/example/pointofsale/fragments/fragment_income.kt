3b86be8fbfae1d2bb03bc5e8d67de466f6625cf3
Shelly Tansil <shellytansil93@gmail.com>
2021-06-05 12:24:09 +0700

    Content Provider - Insert Supplier

--- app/src/main/java/com/example/pointofsale/fragments/fragment_income.kt
+++ app/src/main/java/com/example/pointofsale/fragments/fragment_income.kt
@@ -9,10 +9,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.SearchView
 import android.widget.SimpleCursorAdapter
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.pointofsale.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_income.*
 
 
@@ -56,6 +60,30 @@ class fragment_income : Fragment() {
             readContacts()
         }, 10L)
 
+        val fab = view.findViewById<FloatingActionButton>(R.id.fabContact)
+        fab.setOnClickListener {
+            val views = layoutInflater.inflate(R.layout.form_contact, null, false)
+
+            var dialog = AlertDialog.Builder(view.context)
+                .setView(views)
+                .create()
+
+            dialog.show()
+
+            val bt_add =  views.findViewById<Button>(R.id.bt_add)
+            val nama_supplier =  views.findViewById<EditText>(R.id.nama_supplier)
+            val nomor_supplier =  views.findViewById<EditText>(R.id.nomor_supplier)
+            val email_supplier = views.findViewById<EditText>(R.id.email_supplier)
+
+            bt_add.setOnClickListener {
+                addContact(nama_supplier.text.toString(),
+                    "${nomor_supplier.text}",
+                    "${email_supplier.text}")
+                readContacts()
+                dialog.dismiss()
+            }
+
+        }
         return view
     }
 
@@ -95,26 +123,46 @@ class fragment_income : Fragment() {
         })
     }
 
-    fun addContact(){
-//        val cops = ArrayList<ContentProviderOperation>()
-//
-//        cops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
-//            .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, "accountname@gmail.com")
-//            .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, "com.google")
-//            .build())
-//        cops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-//            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-//            .withValue(ContactsContract.Data.MIMETYPE,
-//                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-//            .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
-//                editTextContactName.getText().toString())
-//            .build())
-//
-//        try {
-//            contentResolver.applyBatch(ContactsContract.AUTHORITY, cops)
-//        } catch (exception: Exception) {
-//            Log.i(TAG, "exception.message")
-//        }
+    private fun removeContacts() {
+//        val whereClause =
+//            ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY + " = '" + editTextContactName.getText()
+//                .toString() + "'"
+//        requireActivity().contentResolver.delete(ContactsContract.RawContacts.CONTENT_URI, whereClause, null)
+    }
+
+    fun addContact(name: String, phone: String, email: String){
+        val cops = ArrayList<ContentProviderOperation>()
+
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+            .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+            .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+            .build())
+
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+            .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+            .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, "Supplier $name")
+            .build());
+
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+            .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+            .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, "$phone")
+            .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+            .build());
+
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+            .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+            .withValue(ContactsContract.CommonDataKinds.Email.DATA, "$email")
+            .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+            .build());
+
+        try {
+            requireActivity().contentResolver.applyBatch(ContactsContract.AUTHORITY, cops)
+        } catch (exception: Exception) {
+            Log.i(TAG, "exception.message")
+        }
     }
     companion object {
         /**
