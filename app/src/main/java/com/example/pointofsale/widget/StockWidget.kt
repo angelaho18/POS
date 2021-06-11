8e02b5b892134ceff92fafff3570cf73482365be
Shelly Tansil <shellytansil93@gmail.com>
2021-06-11 17:30:24 +0700

    Collection Widget Stock - update

--- app/src/main/java/com/example/pointofsale/widget/StockWidget.kt
+++ app/src/main/java/com/example/pointofsale/widget/StockWidget.kt
@@ -3,10 +3,13 @@ package com.example.pointofsale.widget
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.widget.RemoteViews
+import android.widget.Toast
 import com.example.pointofsale.ActivityFragment
 import com.example.pointofsale.EXTRA_NOTIF
 import com.example.pointofsale.R
@@ -15,6 +18,7 @@ import com.example.pointofsale.R
 /**
  * Implementation of App Widget functionality.
  */
+private const val TAG = "STOCK_WIDGET"
 class StockWidget : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
@@ -22,31 +26,50 @@ class StockWidget : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         // There may be multiple widgets active, so update all of them
+        Log.d(TAG, "onUpdate: ")
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
     override fun onEnabled(context: Context) {
+        Log.d(TAG, "onEnabled: ")
         // Enter relevant functionality for when the first widget is created
 
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
-
+        Log.d(TAG, "onDisabled: ")
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
+        Log.d(TAG, "onReceive: ${intent?.action}")
+        val action = intent?.action
+        if (ACTION_REFRESH == action){
+            Log.d(TAG, "onReceive: refresh")
+            Toast.makeText(context, "Sync Product to Aplikasi Kasir Ku", Toast.LENGTH_SHORT).show()
+//            val views = RemoteViews(context?.packageName, R.layout.stock_widget)
+//            val appWidgetId = intent?.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val componentName = ComponentName(context!!.packageName, javaClass.name)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(componentName)
+//            appWidgetManager.updateAppWidget(intent?.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), views)
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list)
+            onUpdate(context, appWidgetManager, appWidgetIds)
+        }
     }
 
     companion object {
+        private const val ACTION_REFRESH = "actionRefresh"
+
         internal fun updateAppWidget(
             context: Context,
             appWidgetManager: AppWidgetManager,
             appWidgetId: Int
         ) {
+            Log.d(TAG, "updateAppWidget:")
             val widgetText = context.getString(R.string.appwidget_text)
 
             // Construct the RemoteViews object
@@ -58,7 +81,6 @@ class StockWidget : AppWidgetProvider() {
             intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
 
             views.setRemoteAdapter(R.id.widget_list, intent)
-//            views.setEmptyView(R.id.widget_list, )
 
             val fragment = Intent(context, ActivityFragment::class.java)
             fragment.apply {
@@ -69,7 +91,15 @@ class StockWidget : AppWidgetProvider() {
                 fragment,
                 PendingIntent.FLAG_UPDATE_CURRENT)
 
+            val refreshIntent = Intent(context, StockWidget::class.java)
+            refreshIntent.action = ACTION_REFRESH
+            val refreshPendingIntent = PendingIntent.getBroadcast(context,
+                0,
+                refreshIntent,
+                0)
+
             views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
+            views.setOnClickPendingIntent(R.id.refresh_btn, refreshPendingIntent)
 
             appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
             // Instruct the widget manager to update the widget
--- app/src/main/java/com/example/pointofsale/widget/StockWidgetService.kt
+++ app/src/main/java/com/example/pointofsale/widget/StockWidgetService.kt
@@ -5,6 +5,7 @@ import android.content.ContentProvider
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
@@ -17,8 +18,7 @@ import kotlinx.android.synthetic.main.stock_widget.view.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-private val TAG = "WIDGET_LIST"
-
+private const val TAG = "STOCK_WIDGET"
 class StockWidgetService: RemoteViewsService() {
     override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
         return StockRemoteViewsFactory(this, intent)
@@ -26,32 +26,36 @@ class StockWidgetService: RemoteViewsService() {
 }
 
 internal class StockRemoteViewsFactory(private val context: Context, intent: Intent?): RemoteViewsService.RemoteViewsFactory {
-    private lateinit var db: ProductDBHelper
+    private var db = ProductDBHelper.getInstance(context)
     private var list = ArrayList<Product>()
 
     override fun onCreate() {
-        db = ProductDBHelper.getInstance(context)!!
         Log.d(TAG, "onCreate: ")
     }
 
     override fun onDataSetChanged() {
-        list.add(Product(1, "Indonmie", 1))
-        list.add(Product(2, "Supermie", 2))
-        list.add(Product(3, "Sprite", 5))
-        list.add(Product(4, "Fanta", 4))
-        doAsync {
-            var datas = db?.productDao()?.getDataForWidget()
-            for (data in datas){
-                list.add(data)
-            }
+//        list.add(Product(1, "Indonmie", 1))
+//        list.add(Product(2, "Supermie", 2))
+//        list.add(Product(3, "Sprite", 5))
+//        list.add(Product(4, "Fanta", 4))
+        list.clear()
+        var datas = db?.productDao()?.getDataForWidget()
+        for (data in datas!!){
+            list.add(data)
         }
+//        SystemClock.sleep(3000)
+        Log.d(TAG, "onDataSetChanged: ")
     }
 
     override fun onDestroy() {
         list.clear()
     }
 
-    override fun getCount(): Int = list.size
+    override fun getCount(): Int {
+        var count = list.size
+        Log.d(TAG, "getCount: $count")
+        return count
+    }
 
     override fun getViewAt(position: Int): RemoteViews {
         val remoteViews = RemoteViews(context.packageName, R.layout.widget_item)
@@ -67,6 +71,7 @@ internal class StockRemoteViewsFactory(private val context: Context, intent: Int
 
         remoteViews.setOnClickFillInIntent(R.id.activity_chooser_view_content, fillInIntent)
 
+        Log.d(TAG, "getViewAt: ")
         return remoteViews
     }
 
